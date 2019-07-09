dd71eb9c04ae7fbdf87d9d46a0167717e74c63f2
Satyanarayana Palakurla <satyanarayana.palakurla@fmglobal.com>
2019-07-08 22:23:48 -0400

    EF

---
+++ EFDbFirstDemo/EFDbFirstDemo/Post.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFDbFirstDemo
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+    }
+}
---
+++ EFDbFirstDemo/EFDbFirstDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace EFDbFirstDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BlogDbContext _context = new BlogDbContext();
+            Console.WriteLine(_context.Blogs.Count());
+            Blog firstBlog = _context.Blogs.FirstOrDefault();
+            Console.WriteLine(firstBlog?.Title);
+
+            Blog blog = _context.Blogs.Where(b => b.Id == 2).FirstOrDefault();
+            Console.WriteLine(blog?.Title);
+
+            //add new entry
+            //Blog blogToAdd = new Blog();
+            //blogToAdd.Title = "Satya Blog";
+            //blogToAdd.Author = "Satya";
+            //blogToAdd.CreatedDt = DateTime.Now;
+            //blogToAdd.CreatedBy = "Satya";
+
+            //_context.Blogs.Add(blogToAdd);
+            //_context.SaveChanges();
+
+            //Update script
+            //Blog blogToUpdate = _context.Blogs.Where(b => b.Id == 3).FirstOrDefault();
+            //blogToUpdate.Title = "blog updated - .net core";
+            //blogToUpdate.UpdatedDt = DateTime.Now;
+            //blogToUpdate.UpdatedBy = "System";
+            //_context.Update(blogToUpdate);
+            //_context.SaveChanges();
+
+
+            //Delete script
+            Blog blogToRemove = _context.Blogs.Where(b => b.Id == 3).FirstOrDefault();
+            _context.Blogs.Remove(blogToRemove);
+            _context.SaveChanges();
+
+
+        }
+    }
+}
